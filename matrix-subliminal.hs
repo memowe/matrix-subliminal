e7aa1ca601c31e526410724cee1dba55c0e3fe6e
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-09-05 10:19:19 +0200

    Parse command line arguments incl. delay via optparse-applicative

--- matrix-subliminal.hs
+++ matrix-subliminal.hs
@@ -1,6 +1,26 @@
 module Main where
 
-import System.Environment
+import Options.Applicative
+
+data Input = Input  { delay     :: Int
+                    , messages  :: [String]
+                    } deriving Show
+
+argsParser :: Parser Input
+argsParser = Input <$> delayP <*> msgsP
+  where delayP  = option auto
+                    (   long "delay"
+                    <>  short 'd'
+                    <>  metavar "MICROSECONDS"
+                    <>  help "Optional delay between updates (default: 80000)"
+                    <>  value 80000
+                    )
+        msgsP   = many (argument str
+                    (   metavar "MESSAGES"
+                    <>  help "Subliminal messages like \"YOLO\" \"The end is near\""
+                    ))
 
 main :: IO ()
-main = mapM_ putStrLn =<< getArgs
+main = do
+  args <- execParser $ info (argsParser <**> helper) fullDesc
+  print args
